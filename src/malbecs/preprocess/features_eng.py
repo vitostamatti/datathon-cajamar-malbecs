24a18d6e97cd45f89245b322edabb227519a75ae
DenisTros <denistrosman@gmail.com>
2023-03-01 17:11:47 +0100

    Agregadas anuales eto

--- src/malbecs/preprocess/features_eng.py
+++ src/malbecs/preprocess/features_eng.py
@@ -23,3 +23,32 @@ def quantiles_col(data,col):
     data = data.merge(quantiles_df[[col,f'{col}_percentiles']], how='left', on = col) #Joineamos con el df transformado
     
     return data
+
+
+#Eto annuals
+
+def eto_dict_generator(df):
+    temp_strings = ['gust','mslp','humidity','uvindex','visibility','windspeed',
+                    'lluvia_daytime','lluvia_night','nieve_day','nieve_night']
+    dic_temp = {}
+    for string in temp_strings:
+        columns = [col for col in df.columns if string in col]
+        dic_temp[string] = columns
+    return dic_temp
+
+
+def eto_annual_sum(df,cols_list):
+    dictionary = eto_dict_generator(df)
+    dictionary = {key: dictionary[key] for key in cols_list}
+    for name, data in dictionary.items():
+            df[name] = df[data].sum(axis=1)
+            df = df.drop(columns=data)
+    return df
+
+def eto_annual_mean(df,cols_list):
+    dictionary = eto_dict_generator(df)
+    dictionary = {key: dictionary[key] for key in cols_list}
+    for name, data in dictionary.items():
+            df[name] = df[data].mean(axis=1)
+            df = df.drop(columns=data)
+    return df
